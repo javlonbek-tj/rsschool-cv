7bd182bf4cc1c87170f0ee5d6781dd911947473d
Жавлон Турдиматов <j.turdimatov@dkp.local>
2024-07-06 19:32:01 +0500

    feat: add index.js for mobile nav

--- js/index.js
+++ js/index.js
@@ -16,3 +16,20 @@ async function copyTextToClipboard() {
 }
 
 copyIcon.addEventListener("click", copyTextToClipboard);
+
+/* Show menu */
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
